601a07c4ac2795a750da56a51bc53e19a9d7017f
severi.tikkanen <severitikkanen@gmail.com>
2019-06-28 11:57:17 +0300

    Removed useless typedef

--- lib/src/screens/PlayerScreen.dart
+++ lib/src/screens/PlayerScreen.dart
@@ -10,7 +10,7 @@ class PlayerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: FutureBuilder<Player>(
+      child: FutureBuilder(
         future: player,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
